2bf539cab5a05532ed7f19e8a86303981209a7b1
JaeYeongSong <songjaey08@gmail.com>
2022-03-09 20:39:13 +0900

    docs : 주석 추가 - 변수의 목적

--- src/main/java/Data_and_operation/Letter.java
+++ src/main/java/Data_and_operation/Letter.java
@@ -2,7 +2,7 @@ package Data_and_operation;
 
 public class Letter {
     public static void main(String[] args) {
-        // 변수를 사용하면 그 값이 무슨 목적으로 사용되었는지 추론할 수 있다
+        // 변수의 목적 - 변수를 사용하면 그 값이 무슨 목적으로 사용되었는지 추론할 수 있다
         String name = "egoing";
         System.out.println("Hello, " + name + " ... " + name + " ... " + "egoing ... bye");
 
