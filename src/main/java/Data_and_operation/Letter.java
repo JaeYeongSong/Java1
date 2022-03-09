9e1692ef623361617a674cb84133abdcb50ad9f8
JaeYeongSong <songjaey08@gmail.com>
2022-03-09 20:38:34 +0900

    feat : 변수의 효용

---
+++ src/main/java/Data_and_operation/Letter.java
@@ -0,0 +1,12 @@
+package Data_and_operation;
+
+public class Letter {
+    public static void main(String[] args) {
+        // 변수를 사용하면 그 값이 무슨 목적으로 사용되었는지 추론할 수 있다
+        String name = "egoing";
+        System.out.println("Hello, " + name + " ... " + name + " ... " + "egoing ... bye");
+
+        double VAT = 10.0;
+        System.out.println(VAT);
+    }
+}
