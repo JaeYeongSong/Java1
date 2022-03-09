1bf3d04bd8ad97602d9ad2f7440c968a30d4a4ee
JaeYeongSong <songjaey08@gmail.com>
2022-03-09 20:05:53 +0900

    feat : 문자열의 표현

---
+++ src/main/java/Data_and_operation/StringApp.java
@@ -0,0 +1,18 @@
+package Data_and_operation;
+
+public class StringApp {
+    public static void main(String[] args) {
+        // Character VS String
+        System.out.println("Hello World"); // String(문자열)
+        System.out.println('H'); // Character(문자)
+        System.out.println("H"); // 사용 가능
+
+        System.out.println("Hello " +
+                "World");
+        // new line
+        System.out.println("Hello\nWorld");
+
+        // escape
+        System.out.println("Hello \"World\""); // Hello "World"
+    }
+}
