e94eaac20d583f48cbe257b26504edc3fef78971
JaeYeongSong <songjaey08@gmail.com>
2022-03-10 11:06:05 +0900

    feat : 나의 앱 만들기

---
+++ src/main/java/MyApp/AccountingApp.java
@@ -0,0 +1,25 @@
+package MyApp;
+
+public class AccountingApp {
+    public static void main(String[] args) {
+        double valueOfSupply = Double.parseDouble(args[0]);
+        double vatRate = 0.1;
+        double expenseRate = 0.3;
+        double VAT = valueOfSupply * vatRate;
+        double total = valueOfSupply + valueOfSupply * vatRate;
+        double expense = valueOfSupply * expenseRate;
+        double income = valueOfSupply - valueOfSupply * expenseRate;
+        double dividend1 = (valueOfSupply - expense) * 0.5;
+        double dividend2 = (valueOfSupply - expense) * 0.3;
+        double dividend3 = (valueOfSupply - expense) * 0.2;
+
+        System.out.println("Value of supply : " + valueOfSupply);
+        System.out.println("VAT : " + VAT);
+        System.out.println("Total : " + total);
+        System.out.println("Expense : " + expense);
+        System.out.println("Income : " + income);
+        System.out.println("Dividend 1 : " + dividend1);
+        System.out.println("Dividend 2 : " + dividend2);
+        System.out.println("Dividend 3 : " + dividend3);
+    }
+}
