e51a5d480a7a42f7e3aea55e8a5be28b949b4be6
JaeYeongSong <songjaey08@gmail.com>
2022-03-10 09:47:41 +0900

    feat : 자바 문서 보는 법 - 클래스
    Class란 무엇인가?

---
+++ src/main/java/Programming/ClassApp.java
@@ -0,0 +1,10 @@
+package Programming;
+
+public class ClassApp {
+    public static void main(String[] args) {
+        // Class는 연관된 변수들이나 메소드를 모아서 Tagging 해놓은 것
+        System.out.println(Math.PI); // Math라는 Class 안에 PI라는 변수
+        System.out.println(Math.floor(1.6)); // Math라는 Class 안에 floor이라는 매소드
+        System.out.println(Math.ceil(1.6)); // Math라는 Class 안에 ceil이라는 매소드
+    }
+}
