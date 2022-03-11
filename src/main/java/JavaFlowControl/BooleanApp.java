5dfb96a15a9b30cdf7cc7476f3122fc06591e22d
JaeYeongSong <songjaey08@gmail.com>
2022-03-11 13:40:35 +0900

    feat : Boolean Datatype

---
+++ src/main/java/JavaFlowControl/BooleanApp.java
@@ -0,0 +1,21 @@
+package JavaFlowControl;
+
+public class BooleanApp {
+    public static void main(String[] args) {
+        // String
+        System.out.println("One");
+
+        // Int
+        System.out.println(1);
+
+        // Boolean
+        System.out.println(true);
+        System.out.println(false);
+
+        String foo = "Hello World";
+        // String true = "Hello World"; // reserved word(예약어)는 변수 이름으로 사용할 수 없다.
+
+        System.out.println(foo.contains("World"));
+        System.out.println(foo.contains("egoing"));
+    }
+}
