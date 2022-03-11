1859627eb1690aa1bc7d24bbfde98434a0f3f446
JaeYeongSong <songjaey08@gmail.com>
2022-03-11 14:16:07 +0900

    feat : 조건문 응용

--- src/main/java/JavaFlowControl/AuthApp.java
+++ src/main/java/JavaFlowControl/AuthApp.java
@@ -5,13 +5,26 @@ public class AuthApp {
         System.out.println(args[0]);
 
         String id = "egoing";
-        String inputId =  args[0];
+        String inputId = args[0];
+
+        String pass = "1111";
+        String inputPass = args[1];
 
         System.out.println("Hi.");
 
-        // if(inputId == id) {
-        if (inputId.equals(id)) {
-            System.out.println("Master!");
+        // // if(inputId == id) {
+        // if (inputId.equals(id)) {
+        //     if (inputPass.equals(pass)) {
+        //         System.out.println("Master!");
+        //     } else {
+        //         System.out.println("Wrong password!");
+        //     }
+        // } else {
+        //     System.out.println("Who are you?");
+        // }
+
+        if (inputId.equals(id) && inputPass.equals(pass)) {
+                System.out.println("Master!");
         } else {
             System.out.println("Who are you?");
         }
