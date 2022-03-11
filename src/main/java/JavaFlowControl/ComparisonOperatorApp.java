1694f61223cc9cc944aa0dc831d397dbf05031ac
JaeYeongSong <songjaey08@gmail.com>
2022-03-11 13:55:19 +0900

    feat : 비교연산자

---
+++ src/main/java/JavaFlowControl/ComparisonOperatorApp.java
@@ -0,0 +1,10 @@
+package JavaFlowControl;
+
+public class ComparisonOperatorApp {
+    public static void main(String[] args) {
+        System.out.println(1 > 1); // false
+        System.out.println(1 == 1); // true
+        System.out.println(1 < 1); // false
+        System.out.println(1 >= 1); // true
+    }
+}
