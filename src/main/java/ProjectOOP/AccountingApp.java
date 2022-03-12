ade8aa36723a5f570174f9e965b65d3075eb8974
JaeYeongSong <songjaey08@gmail.com>
2022-03-12 21:25:32 +0900

    feat : 활용 - 인스턴스화

--- src/main/java/ProjectOOP/AccountingApp.java
+++ src/main/java/ProjectOOP/AccountingApp.java
@@ -1,23 +1,34 @@
 package ProjectOOP;
 
 class Accounting {
-    public static double valueOfSupply; // 공급가액
+    public double valueOfSupply; // 공급가액
     public static double vatRate = 0.1; // 부가가치세율
 
-    public static double getVat() {
-        return valueOfSupply * vatRate;
+    public Accounting(double valueOfSupply) {
+        this.valueOfSupply = valueOfSupply;
     }
 
-    public static double getTotal() {
-        return valueOfSupply + getVat();
+    public double getVat() {
+        return this.valueOfSupply * vatRate;
+    }
+
+    public double getTotal() {
+        return this.valueOfSupply + getVat();
     }
 }
 
 public class AccountingApp {
     public static void main(String[] args) {
-        Accounting.valueOfSupply = 10000.0;
-        System.out.println("Value of supply : " + Accounting.valueOfSupply);
-        System.out.println("VAT : " + Accounting.getVat());
-        System.out.println("Total : " + Accounting.getTotal());
+        Accounting a1 = new Accounting(10000.0);
+        Accounting a2 = new Accounting(20000.0);
+
+        System.out.println("Value of supply : " + a1.valueOfSupply);
+        System.out.println("Value of supply : " + a2.valueOfSupply);
+
+        System.out.println("VAT : " + a1.getVat());
+        System.out.println("VAT : " + a2.getVat());
+
+        System.out.println("Total : " + a1.getTotal());
+        System.out.println("Total : " + a2.getTotal());
     }
 }
