461eced604b825f96d3681ec56bae22bf4379717
JaeYeongSong <songjaey08@gmail.com>
2022-03-12 10:16:53 +0900

    feat : 메소드의 기본 형식

---
+++ src/main/java/PprojectMethod/WhyMethod.java
@@ -0,0 +1,18 @@
+package PprojectMethod;
+
+public class WhyMethod {
+    public static void main(String[] args) {
+        // Code...
+        printTwoTimesA();
+        // Code...
+        printTwoTimesA();
+        // Code...
+        printTwoTimesA();
+    }
+
+    public static void printTwoTimesA() {
+        System.out.println("-");
+        System.out.println("A");
+        System.out.println("A");
+    }
+}
