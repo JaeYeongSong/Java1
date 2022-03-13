93e7cfe753ab1bd34fcf051f30afe9b88a53c0d7
JaeYeongSong <songjaey08@gmail.com>
2022-03-13 13:45:57 +0900

    feat : 기능의 개선과 발전

--- src/main/java/Inheritance/InheritanceApp.java
+++ src/main/java/Inheritance/InheritanceApp.java
@@ -7,7 +7,15 @@ class Cal {
 }
 
 class Cal3 extends Cal{
+    public int minus(int v1, int v2) {
+        return v1 - v2;
+    }
 
+    // Overriding
+    public int sum(int v1, int v2) {
+        System.out.println("Cal3!!");
+        return v1 + v2;
+    }
 }
 
 public class InheritanceApp {
@@ -17,6 +25,7 @@ public class InheritanceApp {
 
         Cal3 c3 = new Cal3();
         System.out.println(c3.sum(2, 1));
+        System.out.println(c3.minus(2, 1));
     }
 }
 
